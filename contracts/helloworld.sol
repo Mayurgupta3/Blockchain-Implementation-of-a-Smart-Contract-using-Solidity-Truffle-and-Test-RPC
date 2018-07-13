f1e884470d5081a1154a3b7ca5e65a36b62181f2
Mayurgupta3 <mgupta7042@gmail.com>
2018-07-14 05:17:11 +0530

    Updated file

--- contracts/helloworld.sol
+++ contracts/helloworld.sol
@@ -3,6 +3,6 @@ contract HelloWorld
 {
     function SayHello() returns (string)
     {
-        return ("Hello World!");
+        return ("Hello World!, This is basic idea of truffle which is an environment for Blockchain");
     }
 }
